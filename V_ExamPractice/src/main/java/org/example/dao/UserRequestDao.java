31e9437c85f2e9e931c68531cd141d60e421bca4
chestarpro <chestarpro@gmail.com>
2021-09-06 00:23:16 +0600

    modified 1.0

---
+++ V_ExamPractice/src/main/java/org/example/dao/UserRequestDao.java
@@ -0,0 +1,17 @@
+package org.example.dao;
+
+import org.example.entity.UserRequestEntity;
+import org.example.model.UserModel;
+
+import java.util.List;
+
+public interface UserRequestDao {
+    UserRequestEntity saveUserRequest(UserRequestEntity userRequestEntity);
+    List<UserRequestEntity> getAllUserRequests();
+    Boolean deleteByUserRequestId(Long id);
+
+    // Создать отдельные точки, которые по Get возвращают всех "Людей" определенного имени, года рождения или пола (поиск)
+    List<UserModel> getByUserName(String name);
+    List<UserModel> getByUserYearOfBirth(Integer yearOfBirth);
+    List<UserModel> getByUserGender(String userGender);
+}
